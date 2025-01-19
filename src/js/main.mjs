e704a6db759aba6b5cee2c81700d7dacdf7df375
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-01-19 12:01:32 +0100

    Improved dark mode transition

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -50,9 +50,12 @@ function initDarkMode() {
     if (localStorage.getItem("theme") === "dark")
         document.body.classList.add("dark-mode");
     document.getElementById("theme-button").addEventListener("click", () => {
+        document.body.classList.add("dark-mode-transition");
         document.body.classList.toggle("dark-mode");
         const theme = (document.body.classList.contains("dark-mode") ? "dark" :
         "light");
         localStorage.setItem("theme", theme);
+        setTimeout(() => document.body.classList.remove("dark-mode-transition"),
+        700);
     });
 }
