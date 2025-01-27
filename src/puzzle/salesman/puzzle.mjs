89267ebbd9c2b6dc89d7aff8da8612db355063e5
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-01-27 22:07:32 +0100

    #24 - Added base functionality of new puzzle

--- src/puzzle/salesman/puzzle.mjs
+++ src/puzzle/salesman/puzzle.mjs
@@ -1,16 +1,26 @@
 
 export { load };
+import { random } from "../../js/random.mjs";
 
-// Graph as adjacency lists (indices of adjecent nodes)
-let graph = null;
 // Size of the graph grid (both width and height). Some nodes are left out
 // depending on next constant
 const graphSize = 4;
 // Number of nodes to generate on the grid
 const nodeCount = 12;
 
+// Graph as adjacency lists (indices of adjecent nodes)
+let graph = null;
+// Current state with properties node (current node) and visited (array of
+// booleans; current node should be visited)
+let state = null;
+// Movement history stored as a stack
+let history = [];
+
 function load() {
     generateGraph();
+    generateInitialState();
+    addInteraction();
+    updateDisplay();
 }
 
 /**
@@ -90,6 +100,7 @@ function displayGraph(positions) {
         child.classList.add("graph-node");
         child.style.left = `${pos[0]}%`;
         child.style.top = `${pos[1]}%`;
+        child.setAttribute("data-node-index", String(i));
         container.appendChild(child);
     }
 }
@@ -110,5 +121,74 @@ function toPercentPosition(pos) {
  * Update the display of the graph
  */
 function updateDisplay() {
+    for (const elt of document.getElementsByClassName("graph-node")) {
+        let index = Number(elt.getAttribute("data-node-index"));
+        // Remove all existing extra classes
+        elt.classList.remove("graph-node-clickable", "graph-node-visited",
+        "graph-node-current");
+        // Current node
+        if (index == state.node)
+            elt.classList.add("graph-node-current");
+        // Adjacent nodes
+        if (graph[state.node].includes(index))
+            elt.classList.add("graph-node-clickable");
+        // Visited nodes
+        if (state.visited[index])
+            elt.classList.add("graph-node-visited");
+    }
+}
 
+/**
+ * Generate an initial state of the game and store it in the state variable
+ */
+function generateInitialState() {
+    let node = Math.floor(random() * graph.length);
+    let visited = [];
+    for (let i = 0; i < graph.length; i++)
+        visited.push(i == node);
+    state = {
+        node: node,
+        visited: visited,
+    };
+}
+
+/**
+ * Create a copy of the current game state
+ * @returns A copy of the current state
+ */
+function copyState() {
+    return JSON.parse(JSON.stringify(state));
+}
+
+/**
+ * Move to a different node. If this is not possible from the current node this
+ * function has no effect. Old state is added to history
+ * @param {number} index The index of the node to move to
+ */
+function moveTo(index) {
+    // First check if move is possible
+    if (!graph[state.node].includes(index))
+        return;
+    history.push(copyState());
+    state.node = index;
+    state.visited[index] = true;
+    updateDisplay();
+}
+
+/**
+ * Undo the last move in the history and update display. Has no effect if there
+ * is nothing in the history
+ */
+function undo() {
+    if (history.length == 0)
+        return;
+    state = history.pop();
+    updateDisplay();
+}
+
+function addInteraction() {
+    for (const elt of document.getElementsByClassName("graph-node")) {
+        let index = Number(elt.getAttribute("data-node-index"));
+        elt.addEventListener("click", () => moveTo(index));
+    }
 }
