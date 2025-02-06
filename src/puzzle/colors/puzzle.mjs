8163e12fc4e50a5ef9a98204b7f976c4735bae44
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-02-06 18:34:17 +0100

    #29 - Started working on generation of initial state and other stuff

--- src/puzzle/colors/puzzle.mjs
+++ src/puzzle/colors/puzzle.mjs
@@ -1,8 +1,11 @@
 
 export { load };
+import { random } from "../../js/random.mjs";
 
 // Maximum number of cells to have in a game state
 const cellCount = 6;
+// Number of random reversed moves to do to generate initial state
+const initialStateMoves = 1000;
 // Definition of all actions
 // Cell states are represented by an index in this array
 const actions = [
@@ -39,8 +42,11 @@ const actions = [
         apply: function (state, index) {
             state = copyState(state);
             let prevIndex = getNextNonEmpty(state, index, -1);
-            if (index != -1)
-                state[prevIndex] = (state[prevIndex] - 2) % 6 + 1;
+            if (index != -1) {
+                state[prevIndex]--;
+                if (state[prevIndex] <= 0)
+                    state[prevIndex] += 6;
+            }
             return state;
         },
         applyReverse: function (state, index) {
@@ -68,15 +74,15 @@ const actions = [
             return this.apply(state, index);
         },
     }, {
-        name: "move_down",
+        name: "move-down",
         color: "green",
         icons: ["arrow_downward"],
         apply: function (state, index) {
             let newState = copyState(state);
             let nextIndex = getNextNonEmpty(state, index);
             if (nextIndex != -1) {
-                newState[index] = newState[nextIndex];
-                newState[nextIndex] = newState[index];
+                newState[index] = state[nextIndex];
+                newState[nextIndex] = state[index];
             }
             return newState;
         },
@@ -84,8 +90,8 @@ const actions = [
             let newState = copyState(state);
             let prevIndex = getNextNonEmpty(state, index, -1);
             if (prevIndex != -1) {
-                newState[index] = newState[prevIndex];
-                newState[prevIndex] = newState[index];
+                newState[index] = state[prevIndex];
+                newState[prevIndex] = state[index];
             }
             return newState;
         },
@@ -96,7 +102,7 @@ const actions = [
         apply: function (state, index) {
             state = copyState(state);
             state[index] = 0;
-            return index;
+            return state;
         },
         applyReverse: function (state, index) {
             return null;
@@ -115,8 +121,11 @@ const actions = [
         applyReverse: function (state, index) {
             state = copyState(state);
             let prevIndex = getNextNonEmpty(state, index, -1);
-            if (prevIndex != -1)
-                state[prevIndex] = (state[prevIndex] - 2) % 6 + 1;
+            if (prevIndex != -1) {
+                state[prevIndex]--;
+                if (state[prevIndex] <= 0)
+                    state[prevIndex] += 6;
+            }
             return state;
         },
     },
@@ -143,6 +152,11 @@ function loadHTML() {
         let elt = document.createElement("div");
         elt.setAttribute("data-cell-index", String(i));
         elt.classList.add("action-button");
+        elt.addEventListener("click", () => {
+            let action = actions[currentState[i]];
+            currentState = action.apply(currentState, i);
+            displayState(currentState);
+        });
         container.append(elt);
     }
 }
@@ -153,12 +167,23 @@ function loadHTML() {
  */
 function displayState(state) {
     let container = document.getElementById("action-button-container");
+    let first = null, last = null;
+    for (let i = 0; i < cellCount; i++) {
+        if (first == null && state[i] != 0)
+            first = i;
+        if (state[i] != 0)
+            last = i;
+    }
     for (let i = 0; i < cellCount; i++) {
         let action = actions[state[i]];
         for (const elt of container.querySelectorAll(`[data-cell-index="${i}"]`))
         {
-            // TODO: Hide empty cells
-            elt.style.backgroundColor = `var(--accent-color-${action.color})`;
+            elt.classList.toggle("action-button-hidden", state[i] == 0);
+            if (state[i] == 0)
+                elt.style.backgroundColor = "var(--soft-background-color)";
+            else
+                elt.style.backgroundColor = (`var(--accent-color-` +
+                `${action.color})`);
             elt.innerHTML = "";
             for (const icon of action.icons) {
                 let iconElt = document.createElement("span");
@@ -166,6 +191,8 @@ function displayState(state) {
                 iconElt.innerText = icon;
                 elt.append(iconElt);
             }
+            elt.classList.toggle("action-button-first", i == first);
+            elt.classList.toggle("action-button-last", i == last);
         }
     }
 }
@@ -174,7 +201,21 @@ function displayState(state) {
  * Generate an initial state to start from by taking actions in reverse
  */
 function generateInitialState() {
-    currentState = [1, 2, 3, 0, 1, 4];
+    // TODO: Properly implement
+    let state = [];
+    for (let i = 0; i < cellCount; i++)
+        state.push(0);
+    let startIndex = Math.floor(random() * state.length);
+    state[startIndex] = 2;
+    for (let i = 0; i < initialStateMoves; i++) {
+        console.log(state);
+        let index = Math.floor(random() * state.length);
+        let result = actions[state[index]].applyReverse(state, index);
+        if (result != null)
+            state = result;
+    }
+    state = actions[state[1]].applyReverse(state, 1);
+    currentState = state;
 }
 
 /**
